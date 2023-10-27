b74482632a23ae21918ec75c84aeb9666657d2bd
pavank <pavankumachr7777@gmail.com>
2023-10-27 17:12:15 +0530

    from ofcpc:Practice

---
+++ UiPath_Assosiate_Cert_Practice/Try_catch_Exception/.local/install/CodedWorkflow.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+
+namespace Try_catch_Exception
+{
+    public partial class CodedWorkflow : CodedWorkflowBase
+    {
+        public CodedWorkflow()
+        {
+            _ = new System.Type[]{};
+        }
+    }
+}
