759ca4b227060ad80cf5a2e7475418da0dec1678
pavank <pavankumachr7777@gmail.com>
2023-11-26 10:44:30 +0530

    ofcpc : practice

---
+++ UIARD_cert_preparation/WAIT_FOR_READY/.local/install/CodedWorkflow.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+
+namespace WAIT_FOR_READY
+{
+    public partial class CodedWorkflow : CodedWorkflowBase
+    {
+        public CodedWorkflow()
+        {
+            _ = new System.Type[]{};
+        }
+    }
+}
